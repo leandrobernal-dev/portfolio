c967e7bb796f94e49bddd4f35cc6173a85f91b82
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-05-04 09:09:52 +0800

    Added siteconfig

---
+++ src/app/config/site-config.js
@@ -0,0 +1,20 @@
+export const siteConfig = {
+    title: "Leandro Bernal",
+    description: "Software Developer",
+    author: "Leandro Bernal",
+    image: "",
+    url: "https://bernalleandro.com",
+    keywords: [
+        "software developer",
+        "full stack developer",
+        "javascript",
+        "react",
+        "nodejs",
+        "nextjs",
+    ],
+    email: "hello@bernalleandro.com",
+    social: {
+        github: "https://github.com/leandrobernal-dev",
+    },
+    copyright: "Copyright © 2024 Leandro Bernal",
+};
